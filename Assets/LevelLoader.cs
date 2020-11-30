b746b7aad5794c61596f850977d0c2550d12ca17
Andy96NNN <julian.andres.garzon@correounivalle.edu.co>
2020-11-30 00:38:44 -0500

    First dev-files upload
    First huge game development files upload.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void LoadLevel(){
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously () {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone) {
+
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            //Debug.Log(operation.progress);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
